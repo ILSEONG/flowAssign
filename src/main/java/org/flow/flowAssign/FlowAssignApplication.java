eaca5f6936150362ada3a6c7befdfd6c4a6c8545
ILESONG <dlftjd9461@naver.com>
2025-06-18 21:05:22 +0900

    프로젝트 생성

---
+++ src/main/java/org/flow/flowAssign/FlowAssignApplication.java
@@ -0,0 +1,13 @@
+package org.flow.flowAssign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowAssignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowAssignApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/flow/flowAssign/FlowAssignApplicationTests.java
@@ -0,0 +1,13 @@
+package org.flow.flowAssign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowAssignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
